b297d7dc6d96fbd7eab4993173d3a1e14749b406
Gunjan Hardasmalani <hardasmalani.g@husky.neu.edu>
2019-12-10 00:31:02 -0500

    Add client service vars in servicesd

--- Arte/src/app/services/client.service.ts
+++ Arte/src/app/services/client.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class ClientService {
 
+  private searchUrl:string;
+  // tslint:disable-next-line: variable-name
+  private client_id ='8173fa018b554d1c8d306e92da2c364b';
+  // tslint:disable-next-line: variable-name
+  private client_secret = '8173fa018b554d1c8d306e92da2c364b';
+  private ArtistUrl: string;
+  private AlbumsUrl: string;
+  private AlbumUrl: string;
+  private encoded = btoa(this.client_id + ':' + this.client_secret);
+
   constructor() { }
 }
