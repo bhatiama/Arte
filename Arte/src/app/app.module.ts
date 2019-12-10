14fcf2b94bdaf3899c1fb556e1de826aac45125a
Gunjan Hardasmalani <hardasmalani.g@husky.neu.edu>
2019-12-09 21:03:01 -0500

    Generate artist component that will contain list of genres

--- Arte/src/app/app.module.ts
+++ Arte/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
