5b30698255d6bac011c47059cc421401754c1d21
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-09 22:55:29 -0500

    logic added for callback and set headers to allow cors

--- Middleware/token-configuration/app.js
+++ Middleware/token-configuration/app.js
@@ -55,12 +55,69 @@ app.get('/login', function (req, res) {
     }));
 });
 
-let key = 'spotify_auth_state';
 
+/**
+ * @function app.use() using path to manage the cookies 
+ */
 var path = require('path');
 app.use(express.static(path.join(__dirname, './public')))
   .use(cookieParser());
 
+/**
+ * @function app.get('/callback') this is the call backpath to which the api will send it to after login
+ * based on the state parameter we will request refresh access tokens
+ */
+app.get('/callback', function (req, res) {
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function (err, res, body) {
+      if (!err && res.statusCode === 200) {
+
+        var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+        // passing token to browser to get request from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
 
 app.listen(port, hostname, () => {
   console.log(`Middleware is up on port ${port}`);
