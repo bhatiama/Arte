99f25c1144c1cf695eb2e5ab38dc728ae880bfbc
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-09 21:39:28 -0500

    added the string generator method to store the state

--- Middleware/token-configuration/app.js
+++ Middleware/token-configuration/app.js
@@ -18,6 +18,24 @@ const express = require('express'), //Using Express framework for application ea
   request = require('request'),
   querystring = require('querystring');
 
+/**
+ * @function stringGenerator
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} text The generated string
+ * to store the state of the application
+ */
+let stringGenerator = (length) => {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+let key = 'spotify_auth_state';
+
 
 app.listen(port, hostname, () => {
   console.log(`Middleware is up on port ${port}`);
