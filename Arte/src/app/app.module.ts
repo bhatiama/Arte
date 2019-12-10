40ffadbde1d56bf37442ab034b46cf2ee7fb0384
Mayur <bhatia.ma@husky.neu.edu>
2019-12-09 22:46:38 -0500

    Create homepage component

--- Arte/src/app/app.module.ts
+++ Arte/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
