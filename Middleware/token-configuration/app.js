7838d87089339d8d9f31f23982e23092181e862a
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-09 23:02:32 -0500

    Added cors headers

--- Middleware/token-configuration/app.js
+++ Middleware/token-configuration/app.js
@@ -37,6 +37,16 @@ let stringGenerator = (length) => {
   }
   return text;
 }
+/**
+ * Setting headers to avoid cors
+ * 
+ */
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+  next();
+})
 
 /**
  * Getting authorization for this application from spotify 
