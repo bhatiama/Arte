9093d49c347ff7073fad8a6047b7100d286fc693
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-11 09:27:35 -0500

    wrapping up the ui minor changes

--- Arte/src/app/components/logout/logout.component.ts
+++ Arte/src/app/components/logout/logout.component.ts
@@ -12,7 +12,10 @@ export class LogoutComponent implements OnInit {
   ngOnInit() {
   }
   Logout() {
-    window.location.href='https://localhost:3000';
+    window.location.href='http://localhost:3000';
     // this.clientService.Logout();
+  }
+  toHome(){
+    window.location.href="http://localhost:4200/tracks"
   }
 }
