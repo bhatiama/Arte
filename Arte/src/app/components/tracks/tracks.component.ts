3f468908848cbeef6f0fefd92e2e7875483189f2
Gunjan Hardasmalani <hardasmalani.g@husky.neu.edu>
2019-12-10 21:37:06 -0500

    Add About component

--- Arte/src/app/components/tracks/tracks.component.ts
+++ Arte/src/app/components/tracks/tracks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-tracks',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TracksComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private clientService: ClientService) { }
+  searchStr: string;
+  searchRes: string[];
   ngOnInit() {
   }
+  searchMusics(){
+    this.clientService.getToken()
+    .subscribe(res => {
+      this.clientService.searchMusics(this.searchStr,'track',res.access_token)
+      .subscribe(res =>{
+        this.searchRes = res.tracks.items;
+    })
+  })
+}
 
 }
